5010de99bf628ecbcee6d625f76ab57c4729afe0
AlvaroEspinozaZ <130480207+AlvaroEspinozaZ@users.noreply.github.com>
2024-05-04 14:01:39 -0500

    ChangesMechanics_ImplementeMoreScenes

--- Assets/Scripts/Player/MovementTest.cs
+++ Assets/Scripts/Player/MovementTest.cs
@@ -6,40 +6,44 @@ public class MovementTest : MonoBehaviour
 {
     [SerializeField] NavMeshAgent nav;
     [SerializeField] Transform final;
+    [SerializeField] Rigidbody rgb;
+    [SerializeField] float impulse=1;
     Vector2 initPos; // Guarda la posición inicial del toque
     bool touchStarted;
     int timer=0;
     private void Awake()
     {
         nav = GetComponent<NavMeshAgent>();
+        rgb = GetComponent<Rigidbody>();
     }
     void Start()
     {
-        nav.SetDestination(final.position);
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveWithTouch();
-        MoveWithKeryboard();
+        //MoveWithKeryboard();
     }
     void MoveWithKeryboard()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
             nav.enabled = false;
-            transform.position = new Vector3(transform.position.x - 3f, transform.position.y, transform.position.z);
+            //transform.position = new Vector3(transform.position.x - 5f, transform.position.y, transform.position.z);
+            rgb.AddForce(Vector3.left* impulse, ForceMode.Impulse);
             nav.enabled = true;
-            nav.SetDestination(final.position);
+            nav.SetDestination(transform.position);
 
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
             nav.enabled = false;
-            transform.position = new Vector3(transform.position.x + 3f, transform.position.y, transform.position.z);
+            //transform.position = new Vector3(transform.position.x + 5f, transform.position.y, transform.position.z);
+            rgb.AddForce(Vector3.left * impulse, ForceMode.Impulse);
             nav.enabled = true;
-            nav.SetDestination(final.position);
+            nav.SetDestination(transform.position);
         }
     }
     void MoveWithTouch()
@@ -61,16 +65,18 @@ public class MovementTest : MonoBehaviour
                 if (direction > 0)
                 {
                     nav.enabled = false;
-                    transform.position = new Vector3(transform.position.x + 3f, transform.position.y, transform.position.z);
+                    //transform.position = new Vector3(transform.position.x + 5f, transform.position.y, transform.position.z);
+                    rgb.AddForce(Vector3.left * impulse, ForceMode.Impulse);
                     nav.enabled = true;
-                    nav.SetDestination(final.position);
+                    //nav.SetDestination(transform.position);
                 }
                 else if (direction < 0)
                 {
                     nav.enabled = false;
-                    transform.position = new Vector3(transform.position.x - 3f, transform.position.y, transform.position.z);
+                    //transform.position = new Vector3(transform.position.x - 5f, transform.position.y, transform.position.z);
+                    rgb.AddForce(Vector3.right * impulse, ForceMode.Impulse);
                     nav.enabled = true;
-                    nav.SetDestination(final.position);
+                    //nav.SetDestination(transform.position);
                 }
 
                 touchStarted = false;
@@ -79,6 +85,7 @@ public class MovementTest : MonoBehaviour
             else if (touch.phase == TouchPhase.Ended)
             {
                 touchStarted = false;
+                rgb.velocity = Vector3.zero;
             }
         }
     }
