f96278d8255d48465d16ae5c5076c948fc8cc296
AlvaroEspinozaZ <130480207+AlvaroEspinozaZ@users.noreply.github.com>
2024-05-06 13:40:53 -0500

    ScenesItems

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public SceneManagementController contrl;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SceneManagementController.cs
+++ Assets/Scripts/SceneManagementController.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class SceneManagementController : MonoBehaviour
+using UnityEngine.SceneManagement;
+[CreateAssetMenu(fileName = "SceneManagementController", menuName = "ScriptableObject/SceneManagementController/Manager")]
+public class SceneManagementController : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    public int EscenaCarga=0;
+    public bool SeCargo=false;
+    private void OnEnable()
     {
-        
+        //Esta solo sirve para cuando se cargar escena y bota el modo de carga, no se para q usarlo
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        //Esta solo me dice si una escena se quita
+        SceneManager.sceneUnloaded += OnSceneUnloaded;
+        //////////////////////////////////////////////////////////////////////
     }
-
-    // Update is called once per frame
-    void Update()
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        
+        SeCargo = true;
+        LoadScene(EscenaCarga);
+        if (SeCargo)
+        {
+            Debug.Log("Se cargó una nueva escena.");
+            Debug.Log("Nombre de la escena: " + scene.name);
+            Debug.Log("Modo de carga: " + mode);
+
+            UnloadScene(EscenaCarga);
+            SeCargo = false;
+        }
+
+    }
+    public void OnSceneUnloaded(Scene scene)
+    {
+        Debug.Log("Se descargó una escena.");
+        Debug.Log("Nombre de la escena: " + scene.name);
+    }
+    public void LoadScene(int SeceneNext)
+    {
+        SceneManager.LoadSceneAsync(SeceneNext, LoadSceneMode.Additive);
+    }
+    public void LoadScene(string SeceneInitName)
+    {
+        SceneManager.LoadSceneAsync(SeceneInitName, LoadSceneMode.Additive);
+    }
+    public void UnloadScene(int SeceneInit)
+    {
+        SceneManager.UnloadSceneAsync(SeceneInit);
+    }
+    public void UnloadScene(string SeceneInitName)
+    {
+        SceneManager.UnloadSceneAsync(SeceneInitName);
+    }
+    public void ChangeScene(int i)
+    {
+        SceneManager.LoadScene(i);
+
+    }
+    private void OnDisable()
+    {
+        //Esta solo sirve para cuando se cargar escena y bota el modo de carga, no se para q usarlo
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        //Esta solo me dice si una escena se quita
+        SceneManager.sceneUnloaded -= OnSceneUnloaded;
     }
 }
